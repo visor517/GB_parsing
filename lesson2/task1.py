db3b33a6649d6086dbc85305ea451dfef8f46ea2
visor517 <visor517@yandex.ru>
2022-01-27 18:55:20 +0300

    lesson2 done

--- lesson2/task1.py
+++ lesson2/task1.py
@@ -6,48 +6,57 @@
 
 import requests
 import re
+import json
 from bs4 import BeautifulSoup
 from pprint import pprint
 
 url = 'https://hh.ru'
-params = {'text': 'Python стажер'}
+params = {'text': 'Python стажер', 'page': 1}
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
-
-response = requests.get(url + '/search/vacancy', params=params, headers=headers)
-
-dom = BeautifulSoup(response.text, 'html.parser')
-
-vacancies_list = dom.find_all('div', {'class': 'vacancy-serp-item__row_header'})
 
 vacancies  = []
 
-for vacancy in vacancies_list:
-    vacancy_data = {}
+while True:
+    response = requests.get(url + '/search/vacancy', params=params, headers=headers)
+    dom = BeautifulSoup(response.text, 'html.parser')
+    vacancies_list = dom.find_all('div', {'class': 'vacancy-serp-item__row_header'})
 
-    title = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})
-    vacancy_data['name'] = title.getText()
-    vacancy_data['link'] = title.get('href')
-    vacancy_data['compensation_min'] = None
-    vacancy_data['compensation_max'] = None
-    vacancy_data['compensation_currency'] = None
-    try:
-        compensation = str(vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).getText())
-        compensation = compensation.replace('\u202f', '')
-        digits = re.findall(r'\d+', compensation, flags=0)
+    if len(vacancies_list) == 0:
+        break
 
-        if compensation.startswith('от'):
-            vacancy_data['compensation_min'] = int(digits[0])
-        elif compensation.startswith('до'):
-            vacancy_data['compensation_max'] = int(digits[0])
-        else:
-            vacancy_data['compensation_min'] = int(digits[0])
-            vacancy_data['compensation_max'] = int(digits[1])
+    for vacancy in vacancies_list:
+        vacancy_data = {}
 
-        vacancy_data['compensation_currency'] = compensation.split(' ')[-1]
+        title = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})
+        vacancy_data['name'] = title.getText()
+        vacancy_data['link'] = title.get('href')
+        vacancy_data['compensation_min'] = None
+        vacancy_data['compensation_max'] = None
+        vacancy_data['compensation_currency'] = None
+        try:
+            compensation = str(vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).getText())
+            compensation = compensation.replace('\u202f', '')
+            digits = re.findall(r'\d+', compensation, flags=0)
 
-    except:
-        pass
+            if compensation.startswith('от'):
+                vacancy_data['compensation_min'] = int(digits[0])
+            elif compensation.startswith('до'):
+                vacancy_data['compensation_max'] = int(digits[0])
+            else:
+                vacancy_data['compensation_min'] = int(digits[0])
+                vacancy_data['compensation_max'] = int(digits[1])
 
-    vacancies.append(vacancy_data)
+            vacancy_data['compensation_currency'] = compensation.split(' ')[-1]
+
+        except:
+            pass
+
+        vacancies.append(vacancy_data)
+
+    params['page'] += 1
     
 pprint(vacancies)
+print(f'Собрано {len(vacancies)} вакансий')
+
+with open('result.json', 'w') as file:
+    json.dump(vacancies, file)
